1c16a64f2c05542502ee8354798e3e298d8b000b
jacktnp <thanaponpkw@gmail.com>
2019-05-22 23:57:32 +0700

    :balloon: add adminpage

--- final_prepare_project/testapp/admin.py
+++ final_prepare_project/testapp/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import Catalog, Shop
+from django.contrib.auth.models import Permission
 
-# Register your models here.
+admin.site.register(Permission)
+
+
+class CatalogAdmin(admin.ModelAdmin):
+  list_display = ['id', 'name']
+
+  fieldsets = [
+    ('ประเภทหนังสือ', {'fields': ['name']})
+  ]
+
+
+admin.site.register(Catalog, CatalogAdmin)
+
+class ShopAdmin(admin.ModelAdmin):
+  list_display = ['id', 'title', 'catalog']
+  list_per_page = 10
+  readonly_fields = ['catalog']
+  search_fields = ['title', 'catalog']
+
+  fieldsets = [
+    ('สินค้า', {'fields': ['title']}),
+    ('แคตาลอก', {'fields': ['catalog']})
+  ]
+
+admin.site.register(Shop, ShopAdmin)
