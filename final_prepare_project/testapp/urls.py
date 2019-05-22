305e6c4fcd0263ca8fb22d8c07c87590edc851c5
jacktnp <thanaponpkw@gmail.com>
2019-05-23 00:08:10 +0700

    :balloon: Login

--- final_prepare_project/testapp/urls.py
+++ final_prepare_project/testapp/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('', views.catalog, name='catalog'),
     path('add/catalog/', views.addcat, name='addcat'),
     path('add/item/', views.addshop, name='addshop'),
+    path('login/', views.auth_login, name='auth_login'),
+    path('register/', views.auth_register, name='auth_register'),
+    path('logout/', views.auth_logout, name='auth_logout'),
 ]
--- final_prepare_project/testapp/views.py
+++ final_prepare_project/testapp/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import logout, login, authenticate
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
@@ -48,3 +49,43 @@ def addshop(request):
 
     context['form'] = form
     return render(request, 'testapp/add-shop.html', context=context)
+
+
+
+def auth_login(request):
+    context = {}
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user:
+            login(request, user)
+
+            next_url = request.POST.get('next_url')
+            if next_url:
+                return redirect(next_url)
+            else:
+                if request.user.is_staff:
+                    return redirect('/admin')
+                else:
+                    return redirect('catalog')
+        else:
+            context['username'] = username
+            context['password'] = password
+            context['error'] = 'Wrong username or password!'
+
+    next_url = request.GET.get('next')
+    if next_url:
+        context['next_url'] = next_url
+
+    return render(request, template_name='testapp/login.html', context=context)
+
+def auth_logout(request):
+	logout(request)
+	return redirect('auth_login')
+
+def auth_register(request):
+    return redirect('auth_login')
